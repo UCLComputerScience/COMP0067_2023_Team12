e62aa402cad6d7dc79f0a1f3eb59701e54ad8bf8
James <james.rudd.jones@gmail.com>
2023-02-15 12:23:14 +0000

    updating buttons and stuff

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Header from './Header';
+import Search from './Search';
 import './App.css';
+
+const sections = [
+  { title: 'Create Projects', url: '#' },
+  { title: 'Edit Projects', url: '#' },
+  { title: 'Create Workbook', url: '#' },
+  { title: 'Log Out & Back To Home', url: '#' },
+];
+
 
 function App() {
   return (
@@ -25,37 +35,40 @@ function App() {
         </a>
       </header>
 
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+      <Header title="Blog" sections={sections} />
 
-    <div className='App-search_bar'>
-      {/* <a class="active" href="#home">Home</a> */}
-      {/* <a href="#about">About</a> */}
-      {/* <a href="#contact">Contact</a> */}
-      <div class="search-container">
-        <form action="/action_page.php">
-          <input class='App-search_bar input' type="text" placeholder="Search.." name="search"/>
-          <button type="submit"><i class="fa fa-search"></i></button>
-        </form>
+      <div className='App-search_bar'>
+        <Search title="Blog" />
       </div>
 
-      <select class="App-category_dropdown" data-style="btn-info" name="selectpicker">
-        <optgroup label="Select Table">
-            <option name="" value="0">Select table</option>
-            <option name="table1" value="1">Table 1</option>
-            <option name="table2" value="2">Table 2</option>
-            <option name="table3" value="3">Table 3</option>
-          </optgroup>
-      </select>
+      <div className='App-search_bar'>
+        {/* <a class="active" href="#home">Home</a> */}
+        {/* <a href="#about">About</a> */}
+        {/* <a href="#contact">Contact</a> */}
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input class='App-search_bar input' type="text" placeholder="Search.." name="search"/>
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
 
-    </div>
+        <select class="App-category_dropdown" data-style="btn-info" name="selectpicker">
+          <optgroup label="Select Table">
+              <option name="" value="0">Select table</option>
+              <option name="table1" value="1">Table 1</option>
+              <option name="table2" value="2">Table 2</option>
+              <option name="table3" value="3">Table 3</option>
+            </optgroup>
+        </select>
 
-    <div className='App-button_bar'>
-      <button class="edit_workbook_button" type="button">Edit</button>
-      <button class="edit_workbook_button" type="button">Delete</button>
-      <button class="edit_workbook_button" type="button">Create Workbook</button>
-    </div>
+      </div>
+
+      <div className='App-button_bar'>
+        <Button variant="contained">Edit</Button>
+        {/* <button class="edit_workbook_button" type="button">Edit</button> */}
+        {/* <button class="edit_workbook_button" type="button">Delete</button> */}
+        {/* <button class="edit_workbook_button" type="button">Create Workbook</button> */}
+      </div>
 
 
     </div>
---
+++ src/Search.js
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import Link from '@mui/material/Link';
+// import useState from 'react';
+import { Select, MenuItem, FormHelperText, FormControl, InputLabel, Box, Toolbar, Button,
+IconButton, Typography, Autocomplete, TextField, Paper, InputBase, Divider, Stack,
+Grid } from '@mui/material'
+
+function Search() {
+//   const { sections, title } = props;
+
+  const [selected, setSelected] = React.useState('');
+
+  const selectionChangeHandler = (event) => {
+    setSelected(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+        <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        {/* <Box className="App"
+            sx={{
+            width: 800,
+            height: 100,
+            margin: '100px auto',
+
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly'
+            }}>
+            <TextField
+                variant='filled'
+                label='Search..'
+            /TextField>
+
+            
+        </Box> */}
+
+        {/* <Stack direction='row' spacing={2}> */}
+
+        <Box
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 800, 
+            backgroundColor: 'white' }}
+            >
+            {/* <IconButton sx={{ p: '10px' }} aria-label="menu">
+                <MenuIcon />
+            </IconButton> */}
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search..."
+                inputProps={{ 'aria-label': 'search' }}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
+            {/* <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions"> */}
+                {/* <SearchIcon /> */}
+            {/* </IconButton> */}
+        </Box>
+
+        <FormControl variant='outlined' style={{ marginTop: 100, marginLeft: 100 }}>
+            <InputLabel>Category</InputLabel>
+            <Select value={selected} onChange={selectionChangeHandler}>
+                <MenuItem value={1}>AI/ML</MenuItem>
+                <MenuItem value={2}>Back-End</MenuItem>
+                <MenuItem value={3}>Cloud</MenuItem>
+                <MenuItem value={4}>Cyber-Security</MenuItem>
+                <MenuItem value={5}>Data Science</MenuItem>
+                <MenuItem value={6}>FinTech</MenuItem>
+                <MenuItem value={7}>Front-End</MenuItem>
+                <MenuItem value={8}>Healthcare</MenuItem>
+                <MenuItem value={9}>Quantum</MenuItem>
+                <MenuItem value={10}>Sustainability</MenuItem>
+            </Select>
+            <FormHelperText>Select a category</FormHelperText>
+        </FormControl>
+
+        {/* </Stack> */}
+
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        <Grid container spacing={0}>
+            <Grid xs={0}>
+                <Button
+                    variant='contained'
+                    size='large'
+                    color="inherit"
+                >
+                    Edit
+                </Button>
+            </Grid>
+            <Grid xs={1}>
+                <Button
+                    variant='contained'
+                    size='large'
+                    color="inherit"
+                >
+                    Delete
+                </Button>
+            </Grid>
+            <Grid xs={10} mdOffset='auto'>
+                <Button
+                    variant='contained'
+                    size='large'
+                    color="inherit"
+                >
+                    Create Workbook
+                </Button>
+            </Grid>
+          
+          
+          
+        </Grid>
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+// Search.propTypes = {
+//   sections: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       title: PropTypes.string.isRequired,
+//       url: PropTypes.string.isRequired,
+//     }),
+//   ).isRequired,
+//   title: PropTypes.string.isRequired,
+// };
+
+export default Search;
