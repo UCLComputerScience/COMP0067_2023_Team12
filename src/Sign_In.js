30cffb0c40fdf1b7b0fee9237b228728a2d0f711
James <james.rudd.jones@gmail.com>
2023-02-21 14:04:19 +0000

    adjusting centering

--- src/Sign_In.js
+++ src/Sign_In.js
@@ -13,6 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from './Header';
+import { Toolbar } from '@mui/material'
 
 const sections = [
     { title: 'Sign In', url: './Sign_In.js'},
@@ -52,68 +53,68 @@ export default function SignIn() {
         
 
     <ThemeProvider theme={theme}>
-      <Header title="Blog" sections={sections} />
-
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar> */}
-          <Typography component="h1" variant="h5">
-            Login as Admin
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-                sx={{input: {textAlign: "center"}}}
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Username"
-                name="email"
-                autoComplete="email"
-                autoFocus
-            />
-            <TextField
-                sx={{textAlign: "right", float:'right'}}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-            />
-            <Grid container>
-            <Grid item xs>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            </Grid>
-            <Grid item>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+        <Header title="Blog" sections={sections} />
+        <Container component="main" maxWidth="xs" justify="flex-end" alignItems="center">  
+        {/* TODO trying to vertically centre the whole thing but is not working with the justify and alignItems */}
+            <CssBaseline />
+            <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
             >
-              Login
-            </Button>
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar> */}
+            <Typography component="h1" variant="h5">
+                Login as Admin
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    sx={{input: {textAlign: "center"}}}
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Username"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                />
+                <TextField
+                    sx={{labelAlign: "right", float:'right'}}
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                />
+                <Grid container>
+                <Grid item xs>
+                <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+                />
+                </Grid>
+                <Grid item>
+                    <Link href="#" variant="body2">
+                    Forgot password?
+                    </Link>
+                </Grid>
+                </Grid>
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                >
+                Login
+                </Button>
             {/* <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
