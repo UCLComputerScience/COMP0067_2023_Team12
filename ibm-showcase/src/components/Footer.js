34a8ce108ff0277adea92eb2892d91a6ebe2663f
Nozomu Kitamura <nozomu.kitamura.21@ucl.ac.uk>
2023-02-27 18:08:09 +0000

    improve footer

--- ibm-showcase/src/components/Footer.js
+++ ibm-showcase/src/components/Footer.js
@@ -1,44 +1,70 @@
 
-import Box from '@mui/material/Box';
-import './HomeBody.css';
-import Toolbar from '@mui/material/Toolbar';
 import './Footer.css';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { Box, Grid} from '@mui/material'; 
+import { Twitter, YouTube, Facebook, LinkedIn } from '@mui/icons-material';
 
 
 function Footer() {
+  const styles = {
+    grid: {
+      mt: "0rem",
+      position: 'static',
+      backgroundColor: '#F4F7FE',
+      justifyContent: 'space-between',
+      alignItems: 'baseline',
+      spacing: 4,
+      p: 2
+    },
+    icon: {
+      fontSize: 30
+    },
+    lockIcon: {
+      fontSize: 15
+    }
+  };
+
   return (
-  <Toolbar style={{mt:"0rem",position:'static',backgroundColor:'#F4F7FE ',justifyContent:'space-between',alignItems:'baseline'}}>
-    <Box className='footer-left'>
-      <ul><b>About</b></ul>
-      <ul>About Program</ul>
-      <ul>About IBM</ul>
-      <ul>About UCL</ul>
-    </Box >
-    <Box className='footer-center-left'>
-    <ul><b>Projects</b></ul>
-      <ul>Featured Projects</ul>
-      <ul>View All Projects</ul>
-      <ul>Search for Projects</ul>
-    </Box >
-    <Box className='footer-center-right'>
-      <ul><b>Contact</b></ul>
-      <ul>Contact Us</ul>
-    </Box>
-    <Box className='footer-center-right'>
-     <ul><b>Follow us</b></ul>
-     <ul>
-        <YouTubeIcon sx={{ fontSize: 30 }} />
-        <TwitterIcon sx={{ fontSize: 30 }}/>
-        <FacebookIcon sx={{ fontSize: 30 }}/>
-        <LinkedInIcon sx={{ fontSize: 30 }}/></ul>
-      <ul  style={{marginTop:"3.5rem"}}>Management Access<LockOpenIcon sx={{ fontSize: 15 }}/></ul>
-    </Box>
-  </Toolbar>
+    <Grid container style={styles.grid}>
+      <Grid item xs={6} sm={6} md={3}>
+        <Box>
+          <ul><b>About</b></ul>
+          <ul>About Program</ul>
+          <ul>About IBM</ul>
+          <ul>About UCL</ul>
+        </Box>
+      </Grid>
+      <Grid item xs={6} sm={6} md={3}>
+        <Box>
+          <ul><b>Projects</b></ul>
+          <ul>Featured Projects</ul>
+          <ul>View All Projects</ul>
+          <ul>Search for Projects</ul>
+        </Box>
+      </Grid>
+      <Grid item xs={6} sm={6} md={3}>
+        <Box>
+          <ul><b>Contact</b></ul>
+          <ul>Contact Us</ul>
+        </Box>
+      </Grid>
+      <Grid item xs={6} sm={6} md={3}>
+        <Box>
+          <ul><b>Follow us</b></ul>
+          <ul>
+            <YouTube sx={styles.icon} alt="YouTube" />
+            <Twitter sx={styles.icon} alt="Twitter" />
+            <Facebook sx={styles.icon} alt="Facebook" />
+            <LinkedIn sx={styles.icon} alt="LinkedIn" />
+          </ul>
+          <ul style={{ marginTop: "3.5rem" }}>
+            Management Access
+            <LockOpenIcon sx={styles.lockIcon} alt="Lock" />
+          </ul>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
+
 export default Footer;
