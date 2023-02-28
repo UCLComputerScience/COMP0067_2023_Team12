869ee730a932438d6e03033bf36657aee260a6ec
Ziyi <rs.x@foxmail.com>
2023-02-28 17:47:01 +0000

    Add Tile Overlay on Hover

--- ibm-showcase/src/components/HomeBody.js
+++ ibm-showcase/src/components/HomeBody.js
@@ -8,13 +8,14 @@ import Button from '@mui/material/Button';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { createTheme } from '@mui/material/styles';
 import PagesIcon from '@mui/icons-material/Pages';
+import React, { useState } from 'react';
 
 function HomeBody() {
   return (
   <section className="HomeBody" style={{margin:'auto', padding:'0 7rem', maxWidth:'110rem'}}>
     <h1 style={{color:'white', fontSize:'4rem', marginBottom:'1rem'}}><PagesIcon sx={{fontSize:'3rem', padding: '0 0.5rem'}}/>Featured Projects</h1>
     <hr style={{margin:'0 0 3rem 0',  height:'1px', color: '#E6E6E6', backgroundColor: '#E6E6E6', border: 'none'}}/>
-    <Box className="TEST" sx={{minHeight: '30.5rem', borderRadius:'1.5rem',backgroundImage:`linear-gradient(90deg, rgba(0,0,0,0.15) 25%, rgba(255,255,255,0) 40%),url(${ProjectPic1})`, backgroundSize: 'cover',backgroundPosition:'center', margin:'0 0 4rem 0', position:'relative'}}>
+    <Box sx={{minHeight: '30.5rem', borderRadius:'1.5rem',backgroundImage:`linear-gradient(90deg, rgba(0,0,0,0.15) 25%, rgba(255,255,255,0) 40%),url(${ProjectPic1})`, backgroundSize: 'cover',backgroundPosition:'center', margin:'0 0 4rem 0', position:'relative'}}>
       <div style={{fontSize: '3rem', color: 'white', padding: '3rem 3rem 1rem 3rem', lineHeight:'90%'}}>UCL Motion Input 3</div>
       <p style={{fontSize: '1.4rem', color: 'white', padding: '0 0 6rem 3rem ', width: '30%', textAlign: 'justify', maxHeight: '50%'}}> 
       Motioninput is a pioneering touchless computing technology devleoped by UCL Computer Science students
@@ -41,8 +42,22 @@ function HomeBody() {
 export default HomeBody;
 
 function ProjectTile(props){
+  const [isShown, setIsShown] = useState(true);
+
   return(
-    <Box sx={{minHeight: '20rem', borderRadius:'1.5rem',backgroundImage:`linear-gradient(0deg, rgba(0,0,0,0.15) 25%, rgba(255,255,255,0) 40%),url(${props.img})`, backgroundSize: 'cover', backgroundPosition:'center', flexGrow: 1, position:'relative'}}>
+    <Box 
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      sx={{width:'30%', minHeight: '20rem', borderRadius:'1.5rem',backgroundImage:`linear-gradient(0deg, rgba(0,0,0,0.15) 25%, rgba(255,255,255,0) 40%),url(${props.img})`, 
+      backgroundSize: 'cover', backgroundPosition:'center', flexGrow: 1, position:'relative',
+      '&:hover': {backgroundImage:`linear-gradient(0deg, rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url(${props.img})`}}}
+    >
+      {isShown && (
+        <section style={{color:'white', padding: '1.5rem 1.5rem 5rem 1.5rem', textAlign: 'justify', fontSize:'1.5rem'}}> 
+        Description Text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </section>)
+      }
+
       <Button size='large' variant="text" 
       sx={{fontSize: '2.2rem', color: 'white', textTransform: "none", padding:'0.5rem 0.5rem', textAlign:'left', margin:'0.5rem 1rem', lineHeight:'1.21', 
            position:'absolute', bottom:'0', fontWeight:'bold'}}
