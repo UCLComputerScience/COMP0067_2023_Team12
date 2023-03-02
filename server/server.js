77ff62dcab713be61885236ad47f55318d1fde5f
JamesRJ <james.rudd.jones@gmail.com>
2023-03-02 17:30:47 +0000

    updating server

--- server/server.js
+++ server/server.js
@@ -20,20 +20,40 @@
 
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://jamesrjrj:pass123@ibmshowcase.kcetsoa.mongodb.net/sample_weatherdata?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri)//, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/api/data', (req, res) => {
-  client.connect(err => {
-    const collection = client.db("sample_weatherdata").collection("data");
-    collection.find({}).toArray((err, data) => {
-      res.send(data);
-    });
-    console.log(collection);
-    console.log('words fam');
-  });
+
 });
+
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+      databasesList = await client.db().admin().listDatabases();
+
+      console.log("Databases:");
+      databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
 
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
