c47469e3821b20e8d8a440475acda3281dc06c18
James <james.rudd.jones@gmail.com>
2023-03-08 19:36:03 +0000

    added register

---
+++ src/Register.js
@@ -0,0 +1,275 @@
+// import * as React from 'react';
+import React, { Component } from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from './components/Header';
+import { Toolbar } from '@mui/material'
+import LoginDataService from "./services/login.service";
+
+const sections = [
+    { title: 'Sign In', url: './Sign_In.js'},
+    { title: 'Create Projects', url: '#' },
+    { title: 'Edit Projects', url: '#' },
+    { title: 'Create Workbook', url: '#' },
+    { title: 'Log Out & Back To Home', url: '#' },
+  ];
+
+
+const theme = createTheme();
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeUser = this.onChangeUser.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.saveProject = this.saveProject.bind(this);
+    this.newProject = this.newProject.bind(this);
+
+    this.state = {
+      id: null,
+      user: "",
+      password: "",
+      published: false,
+
+      labelStateUser: false,
+      labelStatePass: false,
+
+      submitted: false
+    };
+  }
+
+  onChangeUser(e) {
+    this.setState({
+      user: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  saveProject() {
+    // alert("Submitted")
+    var data = {
+      user: this.state.user,
+      password: this.state.password,
+    };
+
+    LoginDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          user: response.data.user,
+          password: response.data.password,
+          
+          published: response.data.published,
+
+          submitted: true
+        });
+        console.log(response.data);
+        // console.log("Submitted");
+        console.log("TESTY SCRIPT")
+        alert(response.data.user);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newProject() {
+    this.setState({
+      id: null,
+      user: "",
+      password: "",
+      published: false,
+
+      submitted: false
+    });
+  }
+
+  handleFocusUser = () => {
+    this.setState({
+      labelStateUser: true,
+    });
+    if (inputRef.current) {
+      const length = inputRef.current.value.length;
+      inputRef.current.selectionStart = length;
+      inputRef.current.selectionEnd = length;
+    }
+  }
+
+  handleBlurUser = (event) => {
+    if (!event.target.value) {
+      this.setState({
+        labelStateUser: false,
+      });
+    }
+  }
+
+
+  handleFocusPass = () => {
+    this.setState({
+      labelStatePass: true,
+    });
+  }
+
+  handleBlurPass = (event) => {
+    if (!event.target.value) {
+      this.setState({
+        labelStatePass: false,
+      });
+    }
+  }
+
+  
+
+  render () {
+    const { labelStateUser } = this.state;
+    const { labelStatePass } = this.state;
+    const
+  return (
+    <ThemeProvider theme={theme}>
+        <Header title="Blog" sections={sections} />
+        <Container component="main" maxWidth="xs" justify="flex-end" alignItems="center" 
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '90vh',
+          // not 100vh since this window height includes the browser search bar and stuff so using 90% instead as this avoids the issue
+        }}>  
+            <CssBaseline />
+            <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+            {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar> */}
+            <Typography component="h1" variant="h5">
+                Register as Admin
+            </Typography>
+            <Box component="form" sx={{ mt: 1 }}>
+                <TextField
+                    // sx={{ '& .MuiInputLabel-root': { textAlign: 'center' } }}
+                    variant='outlined'
+                    sx={{
+                      width: '600px',
+                      '& .MuiOutlinedInput-notchedOutline': {
+                        borderRadius: '50px !important',},
+                      "& .MuiInputLabel-root": {
+                        transform: labelStateUser ? 'translate(14px, -6px) scale(0.75)' : 'translate(265px, 16px) scale(1)',
+                        transformOrigin: 'top left',
+                      },
+                      "& .Mui-focused .MuiInputLabel-root": {
+                        transform: 'translate(14px, -6px) scale(0.75)',
+                        transformOrigin: 'top left',
+                      }
+                    }}
+                    // this is so jank to centre the text but it seems to work so eyo - might need to finalise so it actually is centrerd
+                    margin="normal"
+                    // required
+                    fullWidth
+                    id="email"
+                    label="Username"
+                    name="email"
+                    // autoComplete="email"
+                    value={this.state.user}
+                    onChange={this.onChangeUser}
+                    autoFocus
+
+                    InputLabelProps={{
+                      shrink: labelStateUser,
+                    }}
+                    inputRef={inputRef}
+                    onFocus={this.handleFocusUser}
+                    onBlur={this.handleBlurUser}
+                    inputProps={{
+                      style: {
+                        caretColor: labelStateUser ? 'green' : 'blue',
+                      },
+                      ref: this.inputRef,
+                    }}
+
+                />
+                <TextField
+                    sx={{
+                      width: '600px',
+                      '& .MuiOutlinedInput-notchedOutline': {
+                        borderRadius: '50px !important',},
+                      "& .MuiInputLabel-root": {
+                        transform: labelStatePass ? 'translate(14px, -6px) scale(0.75)' : 'translate(265px, 16px) scale(1)',
+                        transformOrigin: 'top left',
+                      },
+                      "& .Mui-focused .MuiInputLabel-root": {
+                        transform: 'translate(14px, -6px) scale(0.75)',
+                        transformOrigin: 'top left',
+                      }
+                    }}
+                    margin="normal"
+                    // required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    // autoComplete="current-password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+
+                    InputLabelProps={{
+                      shrink: labelStatePass,
+                    }}
+                    onFocus={this.handleFocusPass}
+                    onBlur={this.handleBlurPass}
+                    inputProps={{
+                      style: {
+                        caretColor: labelStatePass ? 'green' : 'blue',
+                      },
+                      ref: this.inputRef,
+                    }}
+                />
+                <Box 
+                sx={{
+                  display: 'flex', 
+                  justifyContent: 'center'
+                }}>
+                <Button
+                // type="submit"
+                // fullWidth
+                variant="contained"
+                sx={{ 
+                  width: '200px',
+                  borderRadius: '50px',
+                  mt: 2, mb: 2,
+                  // display: 'flex', 
+                  justifyContent: 'center' }}
+                onClick={this.saveProject}
+                >
+                Register
+                </Button>
+                </Box>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+}
