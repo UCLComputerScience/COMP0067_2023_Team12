55f7a4cd57880758bbd4582914260726c424e5a9
RSX Ziyi <rs.x@foxmail.com>
2023-03-09 19:20:18 +0000

    Uses James DB

--- ibm-showcase_backend/config/dbConfig.js
+++ ibm-showcase_backend/config/dbConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb+srv://rsx:TEST3210@cluster0.699emwr.mongodb.net/ibm_showcase_db?retryWrites=true&w=majority"
+  url: "mongodb+srv://jamesrjrj:pass234@ibmshowcase.kcetsoa.mongodb.net/ibm_showcase?retryWrites=true&w=majority"
 };
 //ibm_showcase_db is the DB name.
