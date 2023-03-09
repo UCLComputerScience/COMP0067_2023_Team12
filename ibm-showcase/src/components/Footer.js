39071f4a3086ff3a69d6f58885b431b8fc82d91b
Ziyi <rs.x@foxmail.com>
2023-03-09 21:22:29 +0000

    Change Footer Color to #F4F4F4

--- ibm-showcase/src/components/Footer.js
+++ ibm-showcase/src/components/Footer.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom';
 function Footer() {
   const styles = {
     footer: {
-      backgroundColor: '#F4F7FE',
+      backgroundColor: '#F4F4F4',
       display:'flex',
       justifyContent: 'space-around',
       padding: '3rem',
