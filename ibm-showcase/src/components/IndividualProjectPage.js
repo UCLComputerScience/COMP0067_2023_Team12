ae23a9bba71f9192c230ff18c0604c4361096276
Nozomu Kitamura <nozomu.kitamura.21@ucl.ac.uk>
2023-03-15 05:51:05 +0000

    improve individual project page
    need to install react-player,react-slick,
    
    npm install react-slick --save
    npm install slick-carousel --save
    npm i react-player

--- ibm-showcase/src/components/IndividualProjectPage.js
+++ ibm-showcase/src/components/IndividualProjectPage.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './IndividualProjectPage.css';
 import Header from './Header'
 import {ThreeProjectTiles} from './HomeBody'
@@ -5,7 +7,13 @@ import Box from '@mui/material/Box';
 import ProjectPic1 from './Project1.png';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import ProjectPic2 from './Project2.jpeg';
-import { Twitter, YouTube, Facebook, LinkedIn } from '@mui/icons-material';
+import ProjectPic3 from './Project3.png';
+import ProjectPic4 from './Project4.jpg';
+import { Twitter, Reddit, Facebook, LinkedIn, Print } from '@mui/icons-material';
+import IconButton from '@mui/material/IconButton';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function IndividualProjectPage() {
   return (
@@ -21,11 +29,36 @@ function IndividualProjectPage() {
 export default IndividualProjectPage;
 
 function ProjectVideoSection() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const handlePlayClick = () => {
+    setIsPlaying(true);
+  };
+  const handleOnClose = () => {
+    setIsPlaying(false);
+  };
   return (
     <Box sx={{position:'relative',minHeight: '40rem',backgroundImage:`url(${ProjectPic1})`, backgroundSize: 'cover'}}>
-      <PlayCircleIcon sx={{fontSize: '5rem', color: 'white', position:'absolute', left: '50%', top: '50%', transform:`translate(-50%, -50%)`}}/>
-      <div style={{position:'absolute',fontSize: '1.5rem', color: 'white', bottom:'7rem', left: '50%', transform: `translateX(-50%)`}}>Minhaz Hassan / James Rudd Jones / Ziyu Xu / Nozomu Kitamura</div>
-      <div style={{position:'absolute',fontSize: '3rem', color: 'white', bottom:'3rem', left: '50%', transform: `translateX(-50%)`}}>UCL Motion Input 3</div>
+      {isPlaying ? (
+        <div style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}>
+          <ReactPlayer
+            url='https://www.youtube.com/watch?v=JWE4ba_0was&t=1s'
+            playing={true}
+            controls={true}
+            width='100%'
+            height='100%'
+            onEnded={handleOnClose}
+          />
+        </div>
+      ) : (
+        <div>
+          <PlayCircleIcon sx={{fontSize: '5rem', color: 'white', position:'absolute', left: '50%', top: '50%', transform:`translate(-50%, -50%)`,cursor: 'pointer'}} onClick={handlePlayClick}/>
+          <div style={{position:'absolute',fontSize: '1.5rem', color: 'white', bottom:'7rem', left: '50%', transform: `translateX(-50%)`}}>Minhaz Hassan / James Rudd Jones / Ziyu Xu / Nozomu Kitamura</div>
+          <div style={{position:'absolute',fontSize: '3rem', color: 'white', bottom:'3rem', left: '50%', transform: `translateX(-50%)`}}>UCL Motion Input 3</div>
+        </div>
+      )}
+      
+      
+      
     </Box>
   );
 }
@@ -47,6 +80,18 @@ function ProjectDetail() {
 }
 
 function ProjectDescription() {
+  const shareUrl = 'https://www.ibm.com/blogs/think/uk-en/this-is-john-creating-collaborative-projects-with-universities/';
+  const tweetText = 'Check out this innovative project!';
+  const tweethashtags = ['hashtag1', 'hashtag2', 'hashtag3'];
+  const twitterUrl = `https://twitter.com/share?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(shareUrl)}&hashtags=${encodeURIComponent(tweethashtags.join(' #'))}`;
+  const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(shareUrl)}`;
+  const LinkedInUrl = `https://www.linkedin.com/shareArticle?url=` + encodeURIComponent(shareUrl);
+  const redditUrl = `https://www.reddit.com/submit?url=${encodeURIComponent(shareUrl)}`;
+
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
     <div style={{display:'flex', flex:'1'}}>
       <section style={{width:'60%'}}>
@@ -58,10 +103,46 @@ function ProjectDescription() {
         <text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</text>
       </section>
       <section style={{width:'40%',margin:'2rem 0 1rem 1rem'}}>
-        <Box sx={{aspectRatio:'16/10', borderRadius:'1rem',backgroundImage:`url(${ProjectPic2})`, backgroundSize: 'cover', backgroundPosition:'center', position:'relative'}} />
-        <div style={{fontSize:'1.5rem', padding:'2rem'}}>Share on social media: <Twitter style={{fontSize: '3rem'}}/><YouTube style={{fontSize: '3rem'}} /><Facebook style={{fontSize: '3rem'}} /><LinkedIn style={{fontSize: '3rem'}} /></div>
+        <SimpleSlider/>
+        <div style={{fontSize:'1.5rem', padding:'2rem'}}>Share on social media or print: 
+          <IconButton href={twitterUrl} ><Twitter style={{fontSize: '3rem'}}/></IconButton>
+          <IconButton href={facebookUrl}><Facebook style={{fontSize: '3rem'}} /></IconButton>
+          <IconButton href={LinkedInUrl}><LinkedIn style={{fontSize: '3rem'}} /></IconButton>
+          <IconButton href={redditUrl}><Reddit style={{fontSize: '3rem'}} /></IconButton>
+          <IconButton onClick={handlePrint}><Print style={{fontSize: '3rem'}} /></IconButton>
+        </div>
       </section>
     </div>
+  );
+}
+
+function SimpleSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <section style={{margin:'auto', maxWidth:'30rem'}}>
+      <Slider {...settings}>
+        <div>
+          <Box sx={{aspectRatio:'16/10', borderRadius:'1rem',backgroundImage:`url(${ProjectPic1})`, backgroundSize: 'cover', backgroundPosition:'center', position:'relative'}} />
+        </div>
+        <div>
+          <Box sx={{aspectRatio:'16/10', borderRadius:'1rem',backgroundImage:`url(${ProjectPic2})`, backgroundSize: 'cover', backgroundPosition:'center', position:'relative'}} />
+        </div>
+        <div>
+          <Box sx={{aspectRatio:'16/10', borderRadius:'1rem',backgroundImage:`url(${ProjectPic3})`, backgroundSize: 'cover', backgroundPosition:'center', position:'relative'}} />
+        </div>
+        <div>
+          <Box sx={{aspectRatio:'16/10', borderRadius:'1rem',backgroundImage:`url(${ProjectPic4})`, backgroundSize: 'cover', backgroundPosition:'center', position:'relative'}} />
+        </div>
+      </Slider>
+      <p></p>
+    </section>
   );
 }
 
