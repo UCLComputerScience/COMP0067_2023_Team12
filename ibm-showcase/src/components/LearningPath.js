30742d9a45a3c54c45df381231df97de1fe6d936
Nozomu Kitamura <nozomu.kitamura.21@ucl.ac.uk>
2023-04-14 18:14:44 +0100

    make LearingPath responsive

--- ibm-showcase/src/components/LearningPath.js
+++ ibm-showcase/src/components/LearningPath.js
@@ -4,22 +4,25 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import emailjs from '@emailjs/browser';
 import Alert from '@mui/material/Alert';
+import './LearningPath.css';
 
 export default function LearningPath(){
 	return (
-		<div>
+		<div className="Learning" >
+    <div className="LearningBody">
 			<Header />
       <Alert severity="success" icon={false} style={{margin:'2rem 10% 0 10%', fontSize:'1rem'}}>
       Are you a student looking to be part of the programme? Complete the Learning Path below and Register your Interest!
       </Alert>
 
-			<section style={{display:'flex',flexDirection:'row',justifyContent: 'space-around'}}>
-				<Box style={{width:'50%'}}>
-					<Typography variant="h3" style={{fontSize: '3rem', padding:'2rem 0'}}>Learning Path</Typography>
+			<section className="Forms">
+				<Box className="FormLeft" >
+					<Typography variant="h1" sx={{fontSize: '3.5rem', padding:'2rem 0'}}>Learning Path</Typography>
 					<iframe src="https://prezi.com/p/embed/lEJRPrWc3hLGGhVWIt1m?autoplay=1" frameborder='0' allow='autoplay' style={{width:'100%',aspectRatio: '16/9.5'}} ></iframe>
 				</Box>
 				<LearnRight />
 			</section>
+      </div>
 			<Footer />
 		</div>
 	)
@@ -41,14 +44,8 @@ const LearnRight = () => {
   };
 
   const styles = {
-    styleright:{
-      display:'flex',
-      flexDirection:'column',
-      width:"35%",
-      padding:'0 1rem 0 0'
-    },
     abouttitle:{
-      fontSize: '3rem',
+      fontSize: '3.5rem',
       padding:'2rem 0'
     },
     aboutsubtitle:{
@@ -65,7 +62,7 @@ const LearnRight = () => {
     },
   };
   return (
-      <form ref = {form} onSubmit = {sendEmail} style={styles.styleright}>
+      <form ref = {form} onSubmit = {sendEmail} style={styles.styleright} className="FormRight" >
         <Typography variant="h1" style={styles.abouttitle}>Register Interest</Typography>
         <Typography variant="h3" style={styles.aboutsubtitle}>Name</Typography>
         <TextField label="Enter Your Name Here" name="from_name" required style={styles.abouttextfield}/>
