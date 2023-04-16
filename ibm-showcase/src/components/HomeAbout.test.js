7e91ffe8f7c9a823ea012fd9bf925aa8f4bae21e
Nozomu Kitamura <nozomu.kitamura.21@ucl.ac.uk>
2023-04-16 10:26:05 +0100

    unit test

---
+++ ibm-showcase/src/components/HomeAbout.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import HomeAbout from  './HomeAbout';
+
+it('displays the heading', () => {
+    render(
+    <Router>
+      <HomeAbout />
+    </Router>
+    );
+  
+    expect(
+        screen.getByRole('heading', { name: /About/i } )
+      ).toBeInTheDocument()
+  })
+
+it('displays the About', () => {
+  render(
+  <Router>
+    <HomeAbout />
+  </Router>
+  );
+
+  const AboutDescription = screen.getByText(
+    /IBM and UCL have collaborated on various projects over the years. From Artificial Intelligence to Quantum Computing - pushing the boundaries of innovation. Learn more about our story./i
+  );
+  expect(AboutDescription).toBeInTheDocument()
+})
+
+it('displays the get started button', () => {
+  render(
+    <Router>
+      <HomeAbout />
+    </Router>
+    );
+
+  expect(
+    screen.getByRole('button', { name: /Read More/i })
+  ).toBeInTheDocument()
+})
