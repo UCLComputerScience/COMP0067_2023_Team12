5e65f132c18787bfea934a51554509cc1f64d913
Ziyi <rs.x@foxmail.com>
2023-04-18 22:15:35 +0100

    Fix dbConfig to point to the correct DB Name

--- ibm-showcase_backend/config/dbConfig.js
+++ ibm-showcase_backend/config/dbConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   //url: "mongodb+srv://rsx:TEST3210@cluster0.699emwr.mongodb.net/ibm_showcase_db?retryWrites=true&w=majority"
-  url: "mongodb://localhost:27017"
+  url: "mongodb://localhost:27017/ibm_showcase_db"
 };
 //ibm_showcase_db is the DB name.
